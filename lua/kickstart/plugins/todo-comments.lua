05a6856ca598c8f1fa8cd3376ad87ee98336fbf9
Stefano Salvatori <ssalvatori@gmail.com>
2024-09-19 09:25:24 +0200

    add lualine and noice

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,13 @@
 -- Highlight todo, notes, etc in comments
+--
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  event = 'VimEnter',
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
